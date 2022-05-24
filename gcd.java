59b7d6c89fc105c7816ad71cf8dca61928cf8fd5
Kumar Shanu <kumar.shanu2020@vitbhopal.ac.in>
2022-05-25 00:10:32 +0530

    For the time being added gfg code of gcd in it

--- gcd.java
+++ gcd.java
@@ -1,35 +1,42 @@
+
 // Greatest common devisor
 import java.util.Scanner;
+
 public class gcd {
 
-    public static int greatestcd(int a, int b){
-        int quotient = a/b;
-        int remainder = a%b;
+    // Recursive function to return gcd of a and b
+    // code from gfg
+    public static int GCD(int a, int b) {
+        // Everything divides 0
+        if (a == 0)
+            return b;
+        if (b == 0)
+            return a;
 
-        if(remainder==0){
-            return quotient;
-        }
-        else
-        {
-            return greatestcd(b, remainder);
-        }
+        // base case
+        if (a == b)
+            return a;
+
+        // a is greater
+        if (a > b)
+            return GCD(a - b, b);
+        return GCD(a, b - a);
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        while(true){
+        while (true) {
             System.out.println("Enter two numbers:");
             int a = sc.nextInt();
             int b = sc.nextInt();
-            if(a>b)
-                System.out.println("GCD = "+ greatestcd(a,b));
-            else
-                System.out.println("GCD = "+ greatestcd(b,a));
-            
+
+            System.out.println("GCD = " + GCD(a, b));
+
+
             System.out.println("Enter 0 to exit and any other number to continue");
             int temp = sc.nextInt();
-            if(temp == 0)
-            {
+            if (temp == 0) {
                 System.out.println("Program exited successfully");
                 break;
             }
